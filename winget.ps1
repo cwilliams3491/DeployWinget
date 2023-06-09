79f1e7dc39bd23bac836c54078f1d0c9034ccb89
cwilliams3491 <115261292+cwilliams3491@users.noreply.github.com>
2023-06-09 09:19:11 -0400

    Update winget.ps1

--- winget.ps1
+++ winget.ps1
@@ -36,7 +36,7 @@ if (!(Test-Path $InstallerFolder)) { New-Item -Path $InstallerFolder -ItemType D
 			} 
 	
 		#Remove WinGet MSIXBundle 
-		#Remove-Item -Path "$InstallerFolder\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -Force -ErrorAction Continue
+		Remove-Item -Path "$InstallerFolder\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -Force -ErrorAction Continue
 		}
 
         $ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
@@ -45,23 +45,5 @@ if (!(Test-Path $InstallerFolder)) { New-Item -Path $InstallerFolder -ItemType D
         }
     
     $config
-    Set-Location $wingetpath
-	.\winget.exe uninstall "Groove Music"
-	.\winget.exe uninstall "Microsoft.GamingApp_8wekyb3d8bbwe"
-	.\winget.exe uninstall "Xbox TCUI"
-	.\winget.exe uninstall "Xbox Console Companion"
-	.\winget.exe uninstall "Xbox Game Bar Plugin"
-	.\winget.exe uninstall "Xbox Game Bar"
-	.\winget.exe uninstall "Xbox Identity Provider"
-	.\winget.exe uninstall "Xbox Game Speech Window"
-	.\winget.exe uninstall "O365HomePremRetail - en-us"
-	.\winget.exe uninstall "OneNoteFreeRetail - en-us"
-	.\winget.exe install Google.Chrome
-	.\winget.exe install Mozilla.Firefox
-	.\winget.exe install 7zip.7zip
-	.\winget.exe install Adobe.Acrobat.Reader.64-bit
-	.\winget.exe install Microsoft.Teams
-	.\winget.exe install Microsoft.OneDrive
-	.\winget.exe install --id=Microsoft.Office
-    .\winget.exe list
+ 
     
